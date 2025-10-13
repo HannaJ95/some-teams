21caa4d21fa059760d24dc0c4cc3beab019ccea3
Hanna Johansson <hannajohansson16@hotmail.com>
2025-10-13 14:35:06 +0200

    Add code to about.php

---
+++ about.php
@@ -0,0 +1,65 @@
+<?php 
+    $headerImage = "LOGGAUtanNamn.png";
+    require ('header.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        @font-face {
+        font-family: 'Kodchasan-Regular';
+        src: url('resources/Kodchasan/Kodchasan-Regular.ttf') format('truetype');
+        }
+
+        @font-face {
+        font-family: 'Kodchasan-Bold';
+        src: url('resources/Kodchasan/Kodchasan-Bold.ttf') format('truetype');
+        }
+
+        img {
+            display: block;
+            margin: 0 auto; 
+        }
+        body {
+            background-color: #f7e9ca;
+        }
+
+        h1 {
+            font-family: 'Kodchasan-Bold', sans-serif;
+            font-size: 54px;
+            text-align: center;
+            color: #71594aff;
+            margin-top: 0px;
+        }
+
+        h2 {
+            font-family: 'Kodchasan-Regular', sans-serif;
+            color: #71594aff;
+            text-align: center;
+            font-size: 20px;
+        }
+        p {
+            font-family: 'Kodchasan-Regular', sans-serif;
+            color: #71594aff;
+            text-align: center;
+        }
+
+    </style>
+</head>
+<body>
+    <div>
+        <figure>
+            <figcaption>
+                <h1>HANNA JOHANSSON<h1>
+            </figcaption>
+        </figure>
+    </div>
+    <div>
+        <p>Mitt namn är Hanna och jag studerar webbutveckling. Jag gör denna hemsida som en uppgift i min PHP-kurs.</p>
+    </div>
+</body>
+</html>
